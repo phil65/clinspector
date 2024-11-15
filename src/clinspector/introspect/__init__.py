9639db5258b7ba34491b8ce314f60a3341e64b50
Philipp Temminghoff <philipptemminghoff@googlemail.com>
2024-11-15 23:27:50 +0100

    feat: add cleo

--- src/clinspector/introspect/__init__.py
+++ src/clinspector/introspect/__init__.py
@@ -31,6 +31,13 @@ def get_cmd_info(instance: Any) -> commandinfo.CommandInfo | None:
         if isinstance(instance, click.Group):
             return click_info(instance)
 
+    if importlib.util.find_spec("cleo"):
+        from clinspector.introspect.introspect_cleo import get_info as cleo_info
+        from cleo.application import Application
+
+        if isinstance(instance, Application):
+            return cleo_info(instance)
+
     if importlib.util.find_spec("cappa"):
         from clinspector.introspect.introspect_cappa import get_info as cappa_info
 
